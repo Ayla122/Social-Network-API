5a1ece3dc9666e7415780922b3d9205a0c39c24c
Ayla122 <ayladd122@gmail.com>
2021-12-02 18:18:46 -0800

    setup routes/index

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
-// Require express router
 const router = require('express').Router();
 
-// Import all of the API routes 
 const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+    res.status(404).send('<h1>😝 404 Error!</h1>');
+  });
+
+module.exports = router;
